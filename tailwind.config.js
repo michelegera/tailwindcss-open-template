46a8dcb58e07a85ace30ec3639e62eefbe1dae2a
Michele Gerarduzzi <michelegera@users.noreply.github.com>
2020-04-29 23:27:18 +0200

    Remap default colors to custom values

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,21 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': '#151719',
+        'gray-500': '#9ca9b3',
+        'gray-600': '#2a2d32',
+        'gray-700': '#33363a',
+        'gray-800': '#25282c',
+        'green-300': '#73efcc',
+        'green-400': '#1cb68b',
+        'indigo-400': '#acadff',
+        'indigo-500': '#6b6dff',
+        'indigo-600': '#6163ff',
+        'indigo-700': '#5658dd',
+        'white': '#eceded',
+      }
+    },
   },
   variants: {},
   plugins: [
