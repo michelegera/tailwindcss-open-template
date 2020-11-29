505baa8047b8441651331d304c9ff17635611d61
Michele Gerarduzzi <hello@michelegera.dev>
2020-11-29 15:43:51 +0100

    Configure typographic scale with v1 values

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,18 @@ module.exports = {
         700: "#5658dd",
       },
     },
+    fontSize: {
+      xs: "0.75rem",
+      sm: "0.875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+    },
   },
   variants: {},
   plugins: [
