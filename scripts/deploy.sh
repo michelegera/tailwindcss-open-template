76bcffafb44b53670439785ec2be23404747dba3
Michele Gerarduzzi <hello@michelegera.dev>
2020-11-29 16:18:53 +0100

    Add deploy script
    See https://git.io/JkxpU for details.

---
+++ scripts/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+directory=dist
+branch=gh-pages
+
+build_command() {
+  yarn build
+}
+
+echo -e "\033[0;32mDeleting old content...\033[0m"
+rm -rf $directory
+
+echo -e "\033[0;32mChecking out $branch....\033[0m"
+git worktree add $directory $branch
+
+echo -e "\033[0;32mGenerating site...\033[0m"
+build_command
+
+echo -e "\033[0;32mDeploying $branch branch...\033[0m"
+cd $directory &&
+  git add --all &&
+  git commit -m "Deploy updates" &&
+  git push origin $branch
+
+echo -e "\033[0;32mCleaning up...\033[0m"
+git worktree remove $directory
